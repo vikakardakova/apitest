61f3413ef16a7ff83d3d490718bd559fcf9de46d
vikakardakova <vika2893@gmail.com>
2023-10-03 18:42:20 +0300

    almost done with dz about api

--- cypress/e2e/apitestsecond.spec.js
+++ cypress/e2e/apitestsecond.spec.js
@@ -1,32 +1,22 @@
+
 describe("SECOND APP WITH API  ", () => {
   beforeEach(() => {
-    cy.request({
-      method: "POST",
-      url: "http://5.189.186.217/api/auth/login",
-      body: {
-        email: "maysalexandr@gmail.com",
-        password: "123456",
-      },
-    }).then((res) => {
-      const token = res.body.token;
-      console.log(token);
-      cy.visit("http://5.189.186.217/", {
-        onBeforeLoad(win) {
-          win.localStorage.setItem("auth-token", token);
-        },
-      });
-    });
+  cy.LoginApi();
   });
-  it ("Overview open", () => {
+
+
+
+  it ("UI: Overview open", () => {
       cy.visit('http://5.189.186.217/overview');
       cy.contains(" Огляд ");
   })
-  it ("Analytics open", () => {
-      cy.visit('http://5.189.186.217/overview');
-      cy.contains(" Аналітика ").click();
-  })
+  // it ("UI: Analytics open", () => {
+  //     cy.visit('http://5.189.186.217/overview');
+  //     cy.contains(" Аналітика ").click();
+  // })
 
-  it("Api test with category", () => {
+
+  it("API: Open categories", () => {
     cy.request({
       method: "GET",
       url: "http://5.189.186.217/api/category",
@@ -37,24 +27,63 @@ describe("SECOND APP WITH API  ", () => {
   });
 
 
-  it("Api - create category", () => {
+  let _id; 
+  it("API - Create category and save id", () => {
     cy.request({
       method: "POST",
       url: "http://5.189.186.217/api/category",
       headers: {
         Authorization: window.localStorage.getItem("auth-token")
       },
-      body: {
-        name: "Viktoria API TEST"
-      }
+      body: {name: "then delete"}
+    }).then ((res) => {
+      _id = res.body._id;
     });
   });
 
 
-  it("Api - delete category", () => {
+  it("API - Get list of categories", () => {
+    cy.getCategories();
+    cy.get("@categories").then((categories) => {
+      expect(categories.length).to.be.greaterThan(2); // треба виправити і порівняти з юай
+    });
+
+  }); 
+
+
+  it ("UI: Check list of categories", () => {
+    cy.visit('http://5.189.186.217/overview');
+    cy.contains(" Асортимент ").click(); //не розумію як порахувати категоріі на юай і який взагалі флоу? отримання даних з юай та порівняння з апі має бути в одному тесті чи якось інакше?
+
+})
+
+  // it("API - create category without saving ID", () => {
+  //   cy.request({
+  //     method: "POST",
+  //     url: "http://5.189.186.217/api/category",
+  //     headers: {
+  //       Authorization: window.localStorage.getItem("auth-token")
+  //     },
+  //     body: {
+  //       name: "new 03.10"
+  //     }
+  //   });
+  // });
+
+  it("API - delete one particular category", () => {
     cy.request({
       method: "DELETE",
-      url: "http://5.189.186.217/api/category/651ae95b146a28199b52ef16",
+      url: "http://5.189.186.217/api/category/651c158c146a28199b52f61c",
+      headers: {
+        Authorization: window.localStorage.getItem("auth-token")
+      }
+    });
+  });
+
+  it("API - delete category by ID", () => {
+    cy.request({
+      method: "DELETE",
+      url: `http://5.189.186.217/api/category/${_id}`,
       headers: {
         Authorization: window.localStorage.getItem("auth-token")
       }
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,36 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add("LoginApi", () => {
+  cy.request({
+    method: "POST",
+    url: "http://5.189.186.217/api/auth/login",
+    body: {
+      email: "maysalexandr@gmail.com",
+      password: "123456",
+    },
+  }).then((res) => {
+    const token = res.body.token;
+    console.log(token);
+    cy.visit("http://5.189.186.217/", {
+      onBeforeLoad(win) {
+        win.localStorage.setItem("auth-token", token);
+      },
+    });
+  });
+});
+
+
+
+Cypress.Commands.add("getCategories", () => {
+    cy.request({
+      method: "GET",
+      url: "http://5.189.186.217/api/category",
+      headers: {
+        Authorization: window.localStorage.getItem("auth-token"),
+      },
+    }).then((res) => {
+      const categories = res.body;
+      console.log(categories);
+      cy.wrap(categories).as("categories");
+      cy.wrap(res).should("have.property", "status", 200);
+      //cy.wrap(categories).as("categories");
+    });
+  });
