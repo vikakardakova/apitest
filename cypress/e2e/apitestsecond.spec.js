0847f3158970968db11b83a938ee0eb971d589f6
vikakardakova <vika2893@gmail.com>
2023-10-04 23:38:33 +0300

    added env pass and email

--- cypress/e2e/apitestsecond.spec.js
+++ cypress/e2e/apitestsecond.spec.js
@@ -1,85 +1,96 @@
-
 describe("SECOND APP WITH API  ", () => {
   beforeEach(() => {
-  cy.LoginApi();
+    cy.LoginApi();
   });
 
-  // it ("UI: Overview open", () => {
-  //     cy.visit('http://5.189.186.217/overview');
-  //     cy.contains(" Огляд ");
-  // })
-  // it ("UI: Analytics open", () => {
-  //     cy.visit('http://5.189.186.217/overview');
-  //     cy.contains(" Аналітика ").click();
-  // })
+  it("UI: Overview open", () => {
+    cy.visit("http://5.189.186.217/overview");
+    cy.contains(" Огляд ");
+  });
+  it("UI: Analytics open", () => {
+    cy.visit("http://5.189.186.217/overview");
+    cy.contains(" Аналітика ").click();
+  });
 
   it("API: Open categories", () => {
     cy.request({
       method: "GET",
       url: "http://5.189.186.217/api/category",
       headers: {
-        Authorization: window.localStorage.getItem("auth-token")
+        Authorization: window.localStorage.getItem("auth-token"),
       },
     });
   });
 
-
-  // let _id; 
-  // it("API - Create category and save id", () => {
-  //   cy.request({
-  //     method: "POST",
-  //     url: "http://5.189.186.217/api/category",
-  //     headers: {
-  //       Authorization: window.localStorage.getItem("auth-token")
-  //     },
-  //     body: {name: "then delete"}
-  //   }).then ((res) => {
-  //     _id = res.body._id;
-  //   });
-  // });
-
+  let _id;
+  it("API - Create category and save id", () => {
+    cy.request({
+      method: "POST",
+      url: "http://5.189.186.217/api/category",
+      headers: {
+        Authorization: window.localStorage.getItem("auth-token"),
+      },
+      body: { name: "then delete" },
+    }).then((res) => {
+      _id = res.body._id;
+    });
+  });
 
   it("API - Get list of categories and check with UI", () => {
     cy.getCategories(); // get categories by API using created command
-    cy.visit('http://5.189.186.217/overview');
-    cy.get("@categoriesAPI").then((categoriesAPI) => {
-      cy.contains(" Асортимент ").click();
-     // cy.get("[href='/categories']").click();
-      cy.get(".collection > .collection-item").its("length").should("equal", categoriesAPI.length);
+    cy.visit("http://5.189.186.217/overview");
+    cy.get("@categoriesAPI") // беремо збережену змінну, яка відповідаж за респонс по категоріям
+      .then((categoriesAPI) => {
+        cy.contains(" Асортимент ").click();
+        cy.get(".collection > .collection-item") // знаходимо категоріі в юай
+          .its("length")
+          .should("equal", categoriesAPI.length);
       });
-  }); 
+  });
 
+  it("Practicing for memorazing then and its things", () => {
+    cy.getHistory(); //get history using api request
+    cy.visit("http://5.189.186.217/overview");
+    cy.get("@historyAPI") // беремо збережену змінну, яка відповідаж за респонс по історії
+      .then((historyAPI) => {
+        cy.get("[href='/history']").click();
+        cy.contains("Завантажити ще").click();
+        cy.get("tbody > tr") // знаходимо потрібний елемент на юай
+          .its("length")
+          .should("equal", historyAPI.length); //та порівнюємо з апі респонсом
+      });
+  });
 
-  // it("API - create category without saving ID", () => {
-  //   cy.request({
-  //     method: "POST",
-  //     url: "http://5.189.186.217/api/category",
-  //     headers: {
-  //       Authorization: window.localStorage.getItem("auth-token")
-  //     },
-  //     body: {
-  //       name: "new 03.10"
-  //     }
-  //   });
-  // });
+  it("API - create category without saving ID", () => {
+    cy.request({
+      method: "POST",
+      url: "http://5.189.186.217/api/category",
+      headers: {
+        Authorization: window.localStorage.getItem("auth-token"),
+      },
+      body: {
+        name: "new 03.10",
+      },
+    });
+  });
 
-  // it("API - delete one particular category", () => {
-  //   cy.request({
-  //     method: "DELETE",
-  //     url: "http://5.189.186.217/api/category/651c158c146a28199b52f61c",
-  //     headers: {
-  //       Authorization: window.localStorage.getItem("auth-token")
-  //     }
-  //   });
-  // });
+  it("API - delete one particular category", () => {
+    cy.request({
+      method: "DELETE",
+      url: "http://5.189.186.217/api/category/651c158c146a28199b52f61c",
+      headers: {
+        Authorization: window.localStorage.getItem("auth-token"),
+      },
+    });
+  });
 
-//   it("API - delete category by ID", () => {
-//     cy.request({
-//       method: "DELETE",
-//       url: `http://5.189.186.217/api/category/${_id}`,
-//       headers: {
-//         Authorization: window.localStorage.getItem("auth-token")
-//       }
-//     });
-//   });
- });
+  it("API - delete category by ID", () => {
+    cy.request({
+      method: "DELETE",
+      url: `http://5.189.186.217/api/category/${_id}`,
+      headers: {
+        Authorization: window.localStorage.getItem("auth-token"),
+      },
+    });
+  });
+});
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -3,8 +3,10 @@ Cypress.Commands.add("LoginApi", () => {
     method: "POST",
     url: "http://5.189.186.217/api/auth/login",
     body: {
-      email: "maysalexandr@gmail.com",
-      password: "123456",
+      // email: "maysalexandr@gmail.com",
+      // password: "123456",
+      email: Cypress.env("email"),
+      password: Cypress.env("password").toString()
     },
   }).then((res) => {
     const token = res.body.token;
@@ -33,3 +35,19 @@ Cypress.Commands.add("getCategories", () => {
       cy.wrap(res).should("have.property", "status", 200);
     });
   });
+
+
+  Cypress.Commands.add("getHistory", () => {
+    cy.request({
+      method: "GET",
+      url: "http://5.189.186.217/api/order",
+      headers: {
+        Authorization: window.localStorage.getItem("auth-token"),
+      },
+    }).then((res) => {
+      const historyAPI = res.body;
+      console.log(historyAPI);
+      cy.wrap(historyAPI).as("historyAPI");
+      //cy.wrap(res).should("have.property", "status", 200);
+  });
+});
